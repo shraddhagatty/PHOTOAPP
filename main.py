6a8058c9e22212d350bec008e6d58692e230b09a
Ravishanker <ravishanker@Ravishankers-MacBook-Air.local>
2023-10-01 14:26:45 -0500

    Added files

---
+++ main.py
@@ -0,0 +1,191 @@
+#
+# Main program for photoapp program using AWS S3 and RDS to
+# implement a simple photo application for photo storage and
+# viewing.
+#
+# Authors:
+#   YOUR NAME
+#   Prof. Joe Hummel (initial template)
+#   Northwestern University
+#   Fall 2023
+#
+
+import datatier  # MySQL database access
+import awsutil  # helper functions for AWS
+import boto3  # Amazon AWS
+
+import uuid
+import pathlib
+import logging
+import sys
+import os
+
+from configparser import ConfigParser
+
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+
+
+###################################################################
+#
+# prompt
+#
+def prompt():
+  """
+  Prompts the user and returns the command number
+  
+  Parameters
+  ----------
+  None
+  
+  Returns
+  -------
+  Command number entered by user (0, 1, 2, ...)
+  """
+  print()
+  print(">> Enter a command:")
+  print("   0 => end")
+  print("   1 => stats")
+  print("   2 => users")
+  print("   3 => assets")
+  print("   4 => download")
+  print("   5 => download and display")
+  print("   6 => upload")
+  print("   7 => add user")
+
+  cmd = int(input())
+  return cmd
+
+
+###################################################################
+#
+# stats
+#
+def stats(bucketname, bucket, endpoint, dbConn):
+  """
+  Prints out S3 and RDS info: bucket name, # of assets, RDS 
+  endpoint, and # of users and assets in the database
+  
+  Parameters
+  ----------
+  bucketname: S3 bucket name,
+  bucket: S3 boto bucket object,
+  endpoint: RDS machine name,
+  dbConn: open connection to MySQL server
+  
+  Returns
+  -------
+  nothing
+  """
+  #
+  # bucket info:
+  #
+  print("S3 bucket name:", bucketname)
+
+  assets = bucket.objects.all()
+  print("S3 assets:", len(list(assets)))
+
+  #
+  # MySQL info:
+  #
+  print("RDS MySQL endpoint:", endpoint)
+
+  sql = """
+  select now();
+  """
+
+  row = datatier.retrieve_one_row(dbConn, sql)
+  if row is None:
+    print("Database operation failed...")
+  elif row == ():
+    print("Unexpected query failure...")
+  else:
+    print(row[0])
+
+
+#########################################################################
+# main
+#
+print('** Welcome to PhotoApp **')
+print()
+
+# eliminate traceback so we just get error message:
+sys.tracebacklimit = 0
+
+#
+# what config file should we use for this session?
+#
+config_file = 'photoapp-config.ini'
+
+print("What config file to use for this session?")
+print("Press ENTER to use default (photoapp-config.ini),")
+print("otherwise enter name of config file>")
+s = input()
+
+if s == "":  # use default
+  pass  # already set
+else:
+  config_file = s
+
+#
+# does config file exist?
+#
+if not pathlib.Path(config_file).is_file():
+  print("**ERROR: config file '", config_file, "' does not exist, exiting")
+  sys.exit(0)
+
+#
+# gain access to our S3 bucket:
+#
+s3_profile = 's3readwrite'
+
+os.environ['AWS_SHARED_CREDENTIALS_FILE'] = config_file
+
+boto3.setup_default_session(profile_name=s3_profile)
+
+configur = ConfigParser()
+configur.read(config_file)
+bucketname = configur.get('s3', 'bucket_name')
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(bucketname)
+
+#
+# now let's connect to our RDS MySQL server:
+#
+endpoint = configur.get('rds', 'endpoint')
+portnum = int(configur.get('rds', 'port_number'))
+username = configur.get('rds', 'user_name')
+pwd = configur.get('rds', 'user_pwd')
+dbname = configur.get('rds', 'db_name')
+
+dbConn = datatier.get_dbConn(endpoint, portnum, username, pwd, dbname)
+
+if dbConn is None:
+  print('**ERROR: unable to connect to database, exiting')
+  sys.exit(0)
+
+#
+# main processing loop:
+#
+cmd = prompt()
+
+while cmd != 0:
+  #
+  if cmd == 1:
+    stats(bucketname, bucket, endpoint, dbConn)
+  #
+  #
+  # TODO
+  #
+  #
+  else:
+    print("** Unknown command, try again...")
+  #
+  cmd = prompt()
+
+#
+# done
+#
+print()
+print('** done **')
